220901cbb2bbd249c15fc1c2ad659876a478ea70
tuhamworld <52820977+tuhamworld@users.noreply.github.com>
2023-03-06 20:35:42 +0100

    stacking order

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
+let greeting = "Welcome to the Official Website for Tunde Abdulhamid Sanusi";
+alert(greeting);
 
 // Newsletter Script  starts 
 (function ($) { window.fnames = new Array(); window.ftypes = new Array(); fnames[0] = 'EMAIL'; ftypes[0] = 'email'; }(jQuery)); var $mcj = jQuery.noConflict(true);
 
 //  Newsletter Script ends 
-
-//  Welcome message starts
-
-let greeting = "Welcome to the Official Website for Tunde Abdulhamid Sanusi";
-alert(greeting);
-
-// Welcome message ends
