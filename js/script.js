cb1b683250eb6e6ca032ee5b3b695fa4d2e5a830
tuhamworld <52820977+tuhamworld@users.noreply.github.com>
2023-03-07 07:17:04 +0100

    removing the greeting message

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-let greeting = "Welcome to the Official Website for Tunde Abdulhamid Sanusi";
-alert(greeting);
-
 // Newsletter Script  starts 
 (function ($) { window.fnames = new Array(); window.ftypes = new Array(); fnames[0] = 'EMAIL'; ftypes[0] = 'email'; }(jQuery)); var $mcj = jQuery.noConflict(true);
 
