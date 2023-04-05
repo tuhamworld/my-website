a520b1eb5cbc048d8d8e817ea75d2981501dca61
tuhamworld <52820977+tuhamworld@users.noreply.github.com>
2023-04-05 02:44:37 +0100

    adding date object to project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let paraDate = document.querySelector('#date')
+let today = new Date();
+paraDate.textContent = 'Today\'s date is: ' + today.toDateString() + ' | Time: ' + today.getHours() + ':' + today.getMinutes();
 
 // Newsletter Script  starts 
 (function ($) { window.fnames = new Array(); window.ftypes = new Array(); fnames[0] = 'EMAIL'; ftypes[0] = 'email'; }(jQuery)); var $mcj = jQuery.noConflict(true);
