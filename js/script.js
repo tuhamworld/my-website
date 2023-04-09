07a824e6e6cedcf5956dbd63912684ca2da2116d
tuhamworld <52820977+tuhamworld@users.noreply.github.com>
2023-04-09 16:16:14 +0100

    adding ampm to Date and using variables for dateObject()

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 let paraDate = document.querySelector('#date')
 let today = new Date();
-paraDate.textContent = 'Today\'s date is: ' + today.toDateString() + ' | Time: ' + today.getHours() + ':' + today.getMinutes();
+let am = 'am'
+let hour = today.getHours()
+let minutes = today.getMinutes()
+let todayDate = today.toDateString()
+
+if (hour < 12) {
+    am = 'am'
+} else {
+    am = 'pm'
+}
+
+
+paraDate.textContent = 'Today\'s date is: ' + todayDate + ' | Time: ' + hour + ':' + minutes + am;
 
 // Newsletter Script  starts 
 (function ($) { window.fnames = new Array(); window.ftypes = new Array(); fnames[0] = 'EMAIL'; ftypes[0] = 'email'; }(jQuery)); var $mcj = jQuery.noConflict(true);
